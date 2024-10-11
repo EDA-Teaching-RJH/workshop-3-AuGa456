1274279358616792bd5f14b405bc59f302bc78c3
AuGa456 <jsl37@kent.ac.uk>
2024-10-11 10:46:04 +0000

    calculator

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,32 @@
+num1 = int(input("Whats your first number? "))
+num2 = int(input("Whats your second number? "))
+operator = input("Whats your operator? ")
+tf = False
+while tf == False:
+    match operator:
+        case "+":
+            result = num1 + num2
+            print(f"{num1} + {num2} = {result}")
+        case "-":
+            result = num1 - num2
+            print(f"{num1} - {num2} = {result}")
+        case "*":
+            result = num1 * num2
+            print(f"{num1} * {num2} = {result}")
+        case "/":
+            if num1 == 0 or num2 == 0:
+                print("HELL NAW")
+            else:
+                result = num1 / num2
+                print(f"{num1} / {num2} = {result}")
+        case "^":
+            result = num1 ^ num2
+            print(f"{num1} ^ {num2} = {result}")
+        case "%":
+            result = num1 % num2
+            print(f"{num1} % {num2} = {result}")
+        case "Quit":
+            print("Quitting")
+    break
+
 
