ec10e591227a1b049dbe3fb02a058db17355d3ba
AuGa456 <jsl37@kent.ac.uk>
2024-10-11 10:06:43 +0000

    ticket printer

--- partSix.py
+++ partSix.py
@@ -1,1 +1,11 @@
+Age = int(input("How old are you? "))
+Student = input("Are you a student? ")
 
+if Student == "yes" and Age >= 12 or Age <= 64:
+    print("Tickets are £8 please")
+elif Student == "no" and Age >= 12 or Age <=64:
+    print("Tickets are £10 please")
+elif Student == "no" and Age >= 65:
+    print("Tickets are £5 please")
+elif Student == "no" and Age <= 12:
+    print("Tickets are £5 please")
