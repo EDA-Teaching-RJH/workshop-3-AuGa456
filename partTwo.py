3739dbe32ad13636e1627386cb2c02886e8cd0c9
AuGa456 <jsl37@kent.ac.uk>
2024-10-11 09:28:12 +0000

    number guesser

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,10 @@
-
+import random
+secret_number = random.randint(1, 10)
+guessed = False
+while guessed == False:
+    guess = int(input("Guess a number between 1 and 10 "))
+    if guess == secret_number: 
+        print("You habe chosen..... Wisely")
+        guessed == True
+    else: 
+        print("Thats the wrong number silly")
