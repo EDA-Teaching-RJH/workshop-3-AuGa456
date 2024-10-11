ed900e33333ba89b4c9b03dfdfd419752deb25b9
AuGa456 <jsl37@kent.ac.uk>
2024-10-11 09:58:17 +0000

    Day teller

--- partFive.py
+++ partFive.py
@@ -1,1 +1,6 @@
+Day_of_week = input("Give us a day of the week. ")
 
+if Day_of_week == "Saturday" or Day_of_week == "Sunday":
+    print("ITS THE WEEKEND")
+else:
+    print("Its the working week...")
