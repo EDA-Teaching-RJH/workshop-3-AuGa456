6e2bdbd056180fec677becdc0bcf75b674603e08
AuGa456 <jsl37@kent.ac.uk>
2024-10-11 09:41:56 +0000

    grade calculator

--- partThree.py
+++ partThree.py
@@ -1,1 +1,16 @@
+score = int(input("What is your score? "))
 
+if score >= 90 and score <= 100:
+    print("You got an A!")
+elif score >=80 and score <= 89:
+    print("You got a B!")
+elif score >= 70 and score <= 79:
+    print("You got a C")
+elif score >=60 and score <= 69:
+    print("You got a D")
+elif score < 60 and score >= 0:
+    print("You got an F :(")
+elif score > 100:
+    print("Holy cow batman! Thats too much score.")
+else:
+    print("Thats sad, negative score :(")
